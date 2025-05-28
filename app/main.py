cd88d5bd90980170834038afe41c1b9c881fa950
samuel mwalami <samuelmwalami@gmail.com>
2025-05-28 18:52:10 +0300

    added Procfile

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from app.api.routers import blog_router, user_router, auth_router
+from app.database import DatabaseConnection
+from app.config.config import DATABASE_ENGINE
+
+
 
 app = FastAPI()
-
+@app.on_event("startup")
+def on_startup():
+    DatabaseConnection.initialize_database(DATABASE_ENGINE)
+    
 app.include_router(blog_router.router)
 app.include_router(user_router.router)
 app.include_router(auth_router.router)
